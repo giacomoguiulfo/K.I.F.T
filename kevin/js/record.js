f7f9abfcdfa47de0f835d2867ca61da1e86f83b3
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-06-13 05:58:15 -0700

    Client server transfering files

--- kevin/js/record.js
+++ kevin/js/record.js
@@ -1,6 +1,33 @@
 
+
+
+
+
+
+
+
+// record voice
+
 var record = require('node-record-lpcm16')
 var fs = require('fs')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //	fs.unlinkSync('../test.wav')
 function start_recording() {
@@ -13,24 +40,48 @@ function start_recording() {
   }).pipe(file);
 
 
+  setTimeout(function () {
+
+		stats = fs.statSync('../test.wav').size;
+		console.log(stats);
+		client.write(stats.toString());
+		var readStream = fs.createReadStream('../test.wav');
+		readStream.pipe(client);
+  }, 7000)
+
 }
 
-/*
-var http = require("http");
-var server = http.createServer(function(request, response) {
-  //response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("30");
-  // response.write("<html>");
-  // response.write("<head>");
-  // response.write("<title>Hello World Page</title>");
-  // response.write("</head>");
-  // response.write("<body>");
-  // response.write("Hello World!");
-  // response.write("</body>");
-  // response.write("</html>");
-  response.end();
+
+
+
+
+
+
+
+
+
+
+
+// client code
+
+var net = require('net');
+
+var client = new net.Socket();
+client.connect(8500, '127.0.0.1', function() {
+	console.log('Connected');
+
+
+	//client.write('Hello, server! Love, Client.');
+//	console.write('Hello, server! Love, Client.');
 });
 
-server.listen(3000, 'localhost');
-console.log("Server is listening");
-*/
+
+
+client.on('data', function(data) {
+	console.log('Received: ' + data);
+	client.destroy(); // kill client after server's response
+});
+
+client.on('close', function() {
+	console.log('Connection closed');
+});
--- src/server.c
+++ src/server.c
@@ -40,9 +40,9 @@ int main()
 	/* Address family = Internet */
 	serverAddr.sin_family = AF_INET;
 	/* Set port number, using htons function to use proper byte order */
-	serverAddr.sin_port = htons(3000);
+	serverAddr.sin_port = htons(8500);
 	/* Set IP address to localhost */
-	serverAddr.sin_addr.s_addr = inet_addr("localhost");
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	/* Set all bits of the padding field to 0 */
 	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
@@ -63,20 +63,22 @@ int main()
 	recv(newSocket, buffer, 1024, 0);
 	int fd = open("out.wav", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR | S_IXUSR);
 	printf("Testing: %s\n", buffer);
-	return (0);
+
+	/*---- Print the received message ----*/
+	file_size = atoi(buffer);
+	count = 0;
+	printf("Recieved File_Size: %d\n", file_size);
+	bzero(buffer, BUFFER);
+	while(1)
+	{
+		//printf("Data\n");
+		recv(newSocket, buffer, 1024, 0);
+		//write(1, buffer, BUFFER);
+		count += write(fd, buffer, BUFFER);
+		if (count >= file_size)
+			break;
+		bzero(buffer, BUFFER);
+
+	}
+	return 0;
 }
-//
-// 	/*---- Print the received message ----*/
-// 	file_size = atoi(buffer);
-// 	count = 0;
-// 	printf("Recieved File_Size: %d", file_size);
-// 	bzero(buffer, BUFFER);
-// 	while(1)
-// 	{
-// 		recv(newSocket, buffer, 1024, 0);
-// 		count += write(fd, buffer, BUFFER);
-// 		if (count >= file_size)
-// 			break;
-// 	}
-// 	return 0;
-// }
