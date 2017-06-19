eb5516cf56f2321d911a0c0f96cb830201823dfe
gguiulfo <gguiulfo@student.42.us.org>
2017-06-18 17:19:55 -0700

    Improved check

--- src/kift_log.c
+++ src/kift_log.c
@@ -6,7 +6,7 @@
 /*   By: gguiulfo <gguiulfo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/18 04:54:07 by gguiulfo          #+#    #+#             */
-/*   Updated: 2017/06/18 17:07:36 by gguiulfo         ###   ########.fr       */
+/*   Updated: 2017/06/18 17:19:24 by gguiulfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@ void	kift_log(char *recognized, char *response)
 
 	sprintf(buf, "echo \"User: '%s'\" >> kevin_log.txt", recognized);
 	system(buf);
-	sprintf(buf, "echo \"Kevin: '%s'\" >> kevin_log.txt", response);
-	system(buf);
+	if (strcmp(response, "name missing\n"))
+	{
+		sprintf(buf, "echo \"Kevin: '%s'\" >> kevin_log.txt", response);
+		system(buf);
+	}
 }
