ef76d64fa511cca5d77c9b68c310a747638ce747
gguiulfo <gguiulfo@student.42.us.org>
2017-06-18 22:59:05 -0700

    Better dictionary

--- src/kift_extra_cmds.c
+++ src/kift_extra_cmds.c
@@ -6,7 +6,7 @@
 /*   By: gguiulfo <gguiulfo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/18 20:27:08 by gguiulfo          #+#    #+#             */
-/*   Updated: 2017/06/18 22:37:08 by gguiulfo         ###   ########.fr       */
+/*   Updated: 2017/06/18 22:45:03 by gguiulfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,17 @@ int				control_misc(char *cmd, t_server *server)
 	{
 		ft_putbuf("Its very hot in here, I'm close to the CPU", server);
 	}
-	else if (!strcmp(cmd, "EMAIL"))
+	else if (!strcmp(cmd, "E-MAIL"))
 	{
 		ret = system("open -a Thunderbird.app");
 		ft_putbuf("sending email", server);
 	}
 	else if (!strncmp(cmd, "SET ALARM", 9))
-	{
 		ft_putbuf("Alarm set. In five. Four. Three. Two. Juan. Beep Beep Bi",
 					server);
-	}
+	else if (!strncmp(cmd, "KIFT"))
+		ft_putbuf("That is me!", server);
+	else if (!strncmp(cmd, "ALEXA"))
+		ft_putbuf("We used to date. I didn't ended up well", server);
 	return (ret);
 }
