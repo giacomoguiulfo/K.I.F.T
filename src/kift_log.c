5df29b1531f4b72eb1950adc9217456ca09f58f2
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-06-18 17:35:40 -0700

    Updating kift_log.c

--- src/kift_log.c
+++ src/kift_log.c
@@ -12,16 +12,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void	kift_log(char *recognized, char *response)
 {
 	char buf[2048];
 
-	sprintf(buf, "echo \"User: '%s'\" >> kevin_log.txt", recognized);
-	system(buf);
-	if (strcmp(response, "name missing\n"))
+	if (strcmp(response, "name missing"))
 	{
-		sprintf(buf, "echo \"Kevin: '%s'\" >> kevin_log.txt", response);
+		sprintf(buf, "echo \"User: '%s'\" >> kevin_log.txt", recognized);
+		system(buf);
+		sprintf(buf, "echo \"Kevin: '%s'\n\" >> kevin_log.txt", response);
 		system(buf);
 	}
 }
